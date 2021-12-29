3b4d0a6c9acda87ea7487d97ca4a0d5bad5e6f29
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2021-12-29 21:33:27 +0300

    fix: some changes

--- src/components/weather-info/info-searchPanel/SearchPanel.tsx
+++ src/components/weather-info/info-searchPanel/SearchPanel.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const SearchPanel = () => {
     return (
         <div className='pt-10 px-8 pb-6'>
-            <input aria-label='search for place' placeholder='search for place ...'
+            <input aria-label='Search for place' placeholder='Search for place ...'
                 className='transition ease-in-out px-5 py-1 pb-[6px]  
                 bg-transparent bg-black/50 w-full sticky top-0 rounded-full' />
         </div>
