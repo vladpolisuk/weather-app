043561d83ba86a3e422aabe7017fda4d21d18f9e
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-03 19:51:01 +0300

    feature: added skeleton when weather data is loading

--- src/components/weather-preview/preview-city/WeatherCity.tsx
+++ src/components/weather-preview/preview-city/WeatherCity.tsx
@@ -19,12 +19,25 @@ export const WeatherCity = () => {
     return (
         <div className='xl:mt-0 xl:mb-36 xl:p-5 mb-20 mt-5 px-4 flex flex-col items-start justify-start'>
             <div className='flex items-center justify-start'>
-                <CityName name={cityName} />
-                <WeatherIcon iconUrl={weatherIconUrl} description={shortWeather} />
+                {cityName
+                    ? <CityName name={cityName} />
+                    : <div className="animate-pulse h-12 bg-white/20 w-64 rounded mr-2"></div>}
+
+                {weatherIconUrl
+                    ? <WeatherIcon iconUrl={weatherIconUrl} description={shortWeather} />
+                    : <div className="animate-pulse h-12 bg-white/20 w-12 rounded-full"></div>}
             </div>
             <div className='flex'>
-                <CityDate date={localDate} time={localTime} />
-                <CityShortWeather weather={shortWeather} />
+                {localDate || localTime
+                    ? <CityDate date={localDate} time={localTime} />
+                    : <div className='animate-pulse w-48 flex mt-3'>
+                        <div className='w-24 h-6 bg-white/20 rounded mr-1'></div>
+                        <div className='w-24 h-6 bg-white/20 rounded mr-1'></div>
+                    </div>}
+
+                {shortWeather
+                    ? <CityShortWeather weather={shortWeather} />
+                    : <div className="animate-pulse h-6 bg-white/20 w-16 rounded mt-3"></div>}
             </div>
         </div>
     )
