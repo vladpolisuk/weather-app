954113f46178232e8820f24f4c6b677e08557c7b
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-10 16:53:37 +0300

    fix: code refactoring

---
+++ src/components/weather-info/info-searchPanel/searchPanel-searchHelp/searchHelp-similarCityName/SimilarCityName.tsx
@@ -0,0 +1,50 @@
+import React, { FC, HTMLAttributes, KeyboardEvent } from 'react'
+
+interface Props extends HTMLAttributes<HTMLButtonElement> {
+    cityName: string;
+    country: string;
+}
+
+export const SimilarCityName: FC<Props> = ({ cityName, country, ...buttonProps }) => {
+    const onArrowDown = (event: KeyboardEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        const similarCities = document.getElementById('similarCities');
+        const parentLiElement = event.currentTarget.parentNode
+        const nextLiElement = parentLiElement?.nextSibling;
+        if (nextLiElement) (nextLiElement.firstChild as HTMLElement).focus()
+        else (similarCities?.firstChild?.firstChild as HTMLElement).focus();
+    }
+
+    const onArrowUp = (event: KeyboardEvent<HTMLButtonElement>) => {
+        event.preventDefault()
+        const similarCities = document.getElementById('similarCities');
+        const parentLiElement = event.currentTarget.parentNode
+        const prevLiElement = parentLiElement?.previousSibling
+        if (prevLiElement) (prevLiElement.firstChild as HTMLElement).focus()
+        else (similarCities?.lastChild?.firstChild as HTMLElement).focus();
+    }
+
+    const onKeyDown = (event: KeyboardEvent<HTMLButtonElement>) => {
+        if (event.key === 'ArrowDown') onArrowDown(event)
+        else if (event.key === 'ArrowUp') onArrowUp(event)
+    }
+
+    const similarCityNameButtonStyles = `outline-none flex items-center justify-between 
+            w-full px-4 py-2 focus:bg-white/10 hover:bg-white/10 active:bg-white/10`
+
+    const searchHelpOptionStyles = `w-full bg-black/70 mb-[1px]`
+
+    return (
+        <li className={searchHelpOptionStyles}>
+            <button
+                type='submit'
+                {...buttonProps}
+                onKeyDown={onKeyDown}
+                className={similarCityNameButtonStyles}
+                aria-label={`find ${cityName}, ${country}`}>
+                <p className='mr-2'>{cityName}</p>
+                <p className='text-white/70'>{country}</p>
+            </button>
+        </li>
+    )
+}
