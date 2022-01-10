023adf6c52b28349ce21d521e3513c2f5e7cd80b
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-10 16:57:46 +0300

    fix: added check for className property

--- src/components/weather-info/info-dataPanel/dataPanel-infoBox/InfoBox.tsx
+++ src/components/weather-info/info-dataPanel/dataPanel-infoBox/InfoBox.tsx
@@ -2,7 +2,7 @@ import React, { FC, HTMLAttributes } from 'react'
 
 type Props = HTMLAttributes<HTMLDivElement>
 
-export const InfoBox: FC<Props> = ({ children, className }) => {
+export const InfoBox: FC<Props> = ({ children, className = '' }) => {
     return (
         <div className={`relative bg-black/30 px-5 py-3 rounded-md mb-3 w-full ${className}`}>
             {children}
