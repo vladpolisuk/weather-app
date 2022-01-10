3cfbc3cb46d8a2934157d499b2539425a04256ee
Vladislav Polishchuk <vladpolisuk159@gmail.com>
2022-01-11 00:23:07 +0300

    chore: fix work

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,9 @@ import { store } from './store/redux';
 
 const AppProvider = () => {
 	return (
-		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
 	)
 }
 
